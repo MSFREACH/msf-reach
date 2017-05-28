97085804230b2045812bf56da361df5a49346bb2
Tomas <tomasholderness@gmail.com>
2017-05-28 14:16:10 -0400

    testing with unit.js

--- src/test/testCognicityServer.js
+++ src/test/testCognicityServer.js
@@ -1,12 +1,25 @@
 //const request = require('supertest');
-//const assert = require('chai').assert;
-import Promise from 'bluebird';
-import { init } from '../index.js';
+//const assert = require('chai');
+//import Promise from 'bluebird';
+import { init } from '../main.js';
+//import { test } from 'unit.js';
+const test = require('unit.js');
 
 //app = init();
 //app = 1;
 
 describe('GET /user', function() {
+  it('does something', function(done){
+
+    //init().then((app) => {
+
+    //});
+
+    let a = 1;
+    test.value(a).is(1);
+    done();
+  })
+  /*
   it('respond with json', function(done) {
     init().then((app) => {
       request(app)
@@ -15,5 +28,5 @@ describe('GET /user', function() {
         .expect('Content-Type', /json/)
         .expect(200, done);
     });
-  });
+  });*/
 });
