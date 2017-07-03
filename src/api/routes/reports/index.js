c69e7202bf520ec8f7b9118661c63c3c4efe4bb5
Tomas <tomasholderness@gmail.com>
2017-07-03 19:57:05 -0400

    fixed bug where reports not filtered by eventId

--- src/api/routes/reports/index.js
+++ src/api/routes/reports/index.js
@@ -21,7 +21,7 @@ export default ({ config, db, logger }) => {
 				eventId: Joi.number().integer().min(1)
       }
     }),
-		(req, res, next) => reports(config, db, logger).all(req.query.status)
+		(req, res, next) => reports(config, db, logger).all(req.query.eventId)
 			.then((data) => handleGeoResponse(data, req, res, next))
 			.catch((err) => {
 				/* istanbul ignore next */
