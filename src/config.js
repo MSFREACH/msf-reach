053c2a44e1c4e3580eef948a8011610ee346633c
Matthew Berryman <matthew@acrossthecloud.net>
2017-07-08 09:22:22 +1000

    change default key alg

--- src/config.js
+++ src/config.js
@@ -9,7 +9,7 @@ export default {
   API_EVENT_TYPES: (process.env.API_EVENT_TYPES || 'flood,earthquake,conflict').split(','),
   API_REPORT_STATUS_TYPES: (process.env.API_REPORT_STATUS_TYPES || 'confirmed,verified').split(','),
   AWS_COGNITO_PEM: process.env.AWS_COGNITO_PEM.replace(/,/g,'\n') || new Buffer('public_key'),
-  AWS_COGNITO_ALGORITHM: process.env.AWS_COGNITO_ALGORITHM || 'HS256',
+  AWS_COGNITO_ALGORITHM: process.env.AWS_COGNITO_ALGORITHM || 'RS256',
   BODY_LIMIT: process.env.BODY_LIMIT || '100kb',
   CACHE: process.env.CACHE === 'true' || false,
   CACHE_DURATION_CARDS: process.env.CACHE_DURATION_CARDS || '1 minute',
