b71b1ea32b9abb3af217aa6a647981fde375327f
Tomas <tomasholderness@gmail.com>
2017-08-04 10:07:03 -0400

    first work on PDC GeoRSS feed

--- src/api/index.js
+++ src/api/index.js
@@ -13,6 +13,7 @@ import { version } from '../../package.json';
 import events from './routes/events';
 import reports from './routes/reports';
 import twitter from './routes/twitter';
+import hazards from './routes/hazards';
 
 export default ({ config, db, logger }) => {
 	let api = Router();
@@ -25,7 +26,8 @@ export default ({ config, db, logger }) => {
 	// Mount the various endpoints
 	api.use('/events', events({ config, db, logger }));
 	api.use('/reports', reports({ config, db, logger }));
-	api.use('/twitter', twitter({ logger}));
+	api.use('/twitter', twitter({ logger }));
+	api.use('/hazards', hazards({ logger }));
 
 	// Handle validation errors (wording of messages can be overridden using err.isJoi)
 	api.use(validate.errors());
---
+++ src/api/routes/hazards/index.js
@@ -0,0 +1,32 @@
+import Promise from 'bluebird';
+
+import { Router } from 'express';
+
+// Import any required utility functions
+import { cacheResponse, jwtCheck } from '../../../lib/util';
+
+import {GeoRSS} from '../../../lib/pdc-georss.js';
+
+// Import validation dependencies
+import Joi from 'joi';
+import validate from 'celebrate';
+
+export default ({ logger }) => {
+	let api = Router();
+
+	// Get a list of all reports
+	api.get('/', jwtCheck, cacheResponse('10 minutes'),
+		(req, res, next) => GeoRSS()
+			.then((events) => {
+				res.status(200).json({statusCode: 200, result:events})
+			})
+			.catch((err) => {
+				/* istanbul ignore next */
+				logger.error(err);
+				/* istanbul ignore next */
+				next(err);
+			})
+	);
+
+	return api;
+};
