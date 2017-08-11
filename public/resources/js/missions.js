35af2a0d3a0800c11feb3bd6ca72b7a9273801ed
Matthew Berryman <matthew@acrossthecloud.net>
2017-08-11 14:45:13 +1000

    rework tabs for contacts and missions API changes (untested and needs a bit more work

--- public/resources/js/missions.js
+++ public/resources/js/missions.js
@@ -8,7 +8,7 @@ var loadMissions = function(err, missions){
     $('#missionsContainer').append('<table class="table table-striped" id="missionsTable"><thead><tr><th>Name</th><th>Region</th></tr></thead><tbody>');
 
     $.each(missions, function(key, value){
-      $('#missionsTable').append('<tr><td>'+value.properties.name+'</td><td>'+value.properties.region+'</td></tr>');
+      $('#missionsTable').append('<tr><td>'+value.properties.properties.name+'</td><td>'+value.properties.region+'</td></tr>');
     })
 
     $('#missionsTable').append('</tbody></table>');
@@ -18,7 +18,7 @@ var loadMissions = function(err, missions){
 // Perform GET call to get tweets
 var getMissions = function(){
   $.getJSON('/api/missions', function (data){
-    loadMissions(null, data.result[0].missions);
+    loadMissions(null, data.results.features);
   }).fail(function(err){
     loadMissions(err.responseText, null);
   })
