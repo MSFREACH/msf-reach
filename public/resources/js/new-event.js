b6078ee6dabc896bc4915bd34e3403172274b849
Mehrdad Amirghasemi <mehrdad.amirghasemi@gmail.com>
2017-08-13 23:37:21 +1000

    new fields in new events page #30

--- public/resources/js/new-event.js
+++ public/resources/js/new-event.js
@@ -32,61 +32,69 @@ newEventMap.on('click', function(e) {
     latlng = e.latlng; // e is an event object (MouseEvent in this case)
     marker = L.marker(e.latlng).addTo(newEventMap);
 });
+$(function(){
+	$('#createEvent').on('click', function (e) {
 
-$('#createEvent').on('click', function (e) {
-
-    if (latlng === null){
-			$('#newEventModalTitle').html('<h4>Missing event location</h4>');
-			$('#newEventModalContent').html('<p>Please select the epicenter of the event using the map.</p>')
-			$('#newEventModal').modal('toggle');
-    }
-    else {
-      var body = {
-        "status": "active",
-        "type": $('#selectType').val(),
-        "created": new Date().toISOString(),
-        "location": latlng,
-        "metadata":{
-          "user": localStorage.getItem("username"),
-					"name": $("#inputName").val(),
-          "summary": $("#inputSummary").val(),
-          "practical_details": $("#inputPracticals").val(),
-          "security_details": $("#inputSecurity").val(),
-					/* Exploratory details: free text, ask user for
-					 * "Main results of the exploratory mission"
-					 */
-					"exploratory_details": $("#inputExploratoryDetails").val(),
-					"operational_center": $("#inputOpCenter").val(), // check box with only one of OCA/OCBA/OBG/OCB/OCP ticked
-					"other_orgs": $("#inputOtherOrgs").val(), // other organisations, free text
-					"deployment": $("#inputDeployment").val(), // deployment details, free text
-					"capacity": $("#inputCapacity").val(), // capacity, free text
-					/* For next two items have asked for
-					 * Check Boxes + exact number of medical supply + date of arrival
-					 * but not listed anything, so leave as free text
-					 */
-					"medicalMaterials": $("#inputMedicalMaterials").val(),
-					"nonMedicalMaterials": $("#inputNonMedicalMaterials").val(),
-					"population_total": $("#inputPopulationTotal").val(), // total population in area affected
-					"population_affected": $("#inputPopulationAffected").val() // population affected
-					/* Note need to display % but we can
-					* calcuate and display this on events page
-					* from above two variables
-					*/
-        }
-      }
-
-      $.ajax({
-        type: "POST",
-        url: "/api/events",
-        data: JSON.stringify(body),
-        contentType: 'application/json'
-      }).done(function( data, textStatus, req ){
-        var eventId = data.result.objects.output.geometries[0].properties.id;
-        window.location.href = '/events/?eventId='+eventId;
-      }).fail(function (reqm, textStatus, err){
-				$('#newEventModalTitle').html('<h4>Error creating event</h4>');
-				$('#newEventModalContent').html('<p>' + err +'.</p>')
+	    if (latlng === null){
+				$('#newEventModalTitle').html('<h4>Missing event location</h4>');
+				$('#newEventModalContent').html('<p>Please select the epicenter of the event using the map.</p>')
 				$('#newEventModal').modal('toggle');
-      });
-    }
-})
+	    }
+	    else {
+	      var body = {
+	        "status": "active",
+	        "type": $('#selectType').val(),
+	        "created": new Date().toISOString(),
+	        "location": latlng,
+	        "metadata":{
+	          "user": localStorage.getItem("username"),
+						"name": $("#inputName").val(),
+	          "summary": $("#inputSummary").val(),
+	          "practical_details": $("#inputPracticals").val(),
+	          "security_details": $("#inputSecurity").val(),
+						/* Exploratory details: free text, ask user for
+						 * "Main results of the exploratory mission"
+						 */
+						"exploratory_details": $("#inputExploratoryDetails").val(),
+						"operational_center": $("#inputOpCenter").val(), // check box with only one of OCA/OCBA/OBG/OCB/OCP ticked
+						"other_orgs": $("#inputOtherOrgs").val(), // other organisations, free text
+						"deployment": $("#inputDeployment").val(), // deployment details, free text
+						"capacity": $("#inputCapacity").val(), // capacity, free text
+						/* For next two items have asked for
+						 * Check Boxes + exact number of medical supply + date of arrival
+						 * but not listed anything, so leave as free text
+						 */
+						"medicalMaterials": $("#inputMedicalMaterials").val(),
+						"nonMedicalMaterials": $("#inputNonMedicalMaterials").val(),
+						"population_total": $("#inputPopulationTotal").val(), // total population in area affected
+						"population_affected": $("#inputPopulationAffected").val() // population affected
+						/* Note need to display % but we can
+						* calcuate and display this on events page
+						* from above two variables
+						*/
+	        }
+	      }
+
+	      $.ajax({
+	        type: "POST",
+	        url: "/api/events",
+	        data: JSON.stringify(body),
+	        contentType: 'application/json'
+	      }).done(function( data, textStatus, req ){
+	        var eventId = data.result.objects.output.geometries[0].properties.id;
+	        window.location.href = '/events/?eventId='+eventId;
+	      }).fail(function (reqm, textStatus, err){
+					$('#newEventModalTitle').html('<h4>Error creating event</h4>');
+					$('#newEventModalContent').html('<p>' + err +'.</p>')
+					$('#newEventModal').modal('toggle');
+	      });
+	    }
+	});
+
+$('#nextEventTab').on('click',function(){
+	console.log('hi');
+	$('#extraTab').tab('show');
+});
+
+
+});
