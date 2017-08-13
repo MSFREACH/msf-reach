9cc6bb57a69273d49a46b2fbd7b162f2b3158816
Matthew Berryman <matthew@acrossthecloud.net>
2017-08-14 09:00:07 +1000

    handle case where population isn't specified

--- public/resources/js/landing.js
+++ public/resources/js/landing.js
@@ -30,29 +30,36 @@ var getAllEvents = function(callback){
 
 // Add popups
 function onEachFeature(feature, layer) {
-  var percentagePopulation = String(Math.round(Number(feature.properties.metadata.population_affected)/Number(feature.properties.metadata.population_total)*100));
+  var percentagePopulation;
+  if (feature.properties.metadata.population_affected && feature.properties.metadata.population_total) {
+    percentagePopulation = String(Math.round(Number(feature.properties.metadata.population_affected)/Number(feature.properties.metadata.population_total)*100));
+  }
   var popupContent = "<strong><a href='events/?eventId=" + feature.properties.id +
     "'>Event " + feature.properties.id +"</a></strong>" +
     "<BR>Status: " + feature.properties.status +
     "<BR>Type: " + feature.properties.type +
-    "<BR>Created: " + feature.properties.created +
-    "<BR>Total population: " + feature.properties.metadadata.population_total +
+    "<BR>Created: " + feature.properties.created;
+  if (percentagePopulation) {
+    popupContent += "<BR>Total population: " + feature.properties.metadadata.population_total +
     "<BR>% pop. affected: " + percentagePopulation;
+  }
   $('#eventProperties').append(
     '<p>' +
     'Name: <a href="/events/?eventId=' + feature.properties.id + '">' + feature.properties.metadata.name + '</a><br>' +
     'Type: ' + feature.properties.type + '<br>' + // needs conversion
     'Status: ' + feature.properties.status + '<br>' +
-    'Created: ' + feature.properties.created + '<br>' +
-    'Total population: ' + feature.properties.metadata.population_total + '<br>' +
-    '% pop. affected: ' + percentagePopulation +
-    '</p>');
-    if (feature.properties && feature.properties.popupContent) {
-      popupContent += feature.properties.popupContent;
-    }
-
-    layer.bindPopup(popupContent);
+    'Created: ' + feature.properties.created);
+  if (percentagePopulation) {
+    $('#eventProperties').append(
+      '<br>Total population: ' + feature.properties.metadata.population_total + '<br>' +
+      '% pop. affected: ' + percentagePopulation +
+      '</p>');
+  if (feature.properties && feature.properties.popupContent) {
+    popupContent += feature.properties.popupContent;
   }
+
+  layer.bindPopup(popupContent);
+}
 
   // MSF Icons
   var msfIcon = L.icon({
