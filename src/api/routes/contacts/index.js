245bbac48a2ac5e0c5475d1ee80882ed014a02b6
Mehrdad Amirghasemi <mehrdad.amirghasemi@gmail.com>
2017-08-20 14:37:23 +1000

    extending the contact API for create and updating contacts

--- src/api/routes/contacts/index.js
+++ src/api/routes/contacts/index.js
@@ -32,5 +32,48 @@ export default ({ config, db, logger }) => {
 			})
 	);
 
+	// Create a new contact record in the database
+	api.post('/',
+		validate({
+			body: Joi.object().keys({
+				properties: Joi.object().required(),
+				location: Joi.object().required().keys({
+					lat: Joi.number().min(-90).max(90).required(),
+					lng: Joi.number().min(-180).max(180).required()
+				})
+			})
+		}),
+		(req, res, next) => {
+			contacts(config, db, logger).createContact(req.body)
+			.then((data) => handleGeoResponse(data, req, res, next))
+				.catch((err) => {
+					/* istanbul ignore next */
+					logger.error(err);
+					/* istanbul ignore next */
+					next(err);
+				})
+		}
+	);
+
+	// Update a contact record in the database
+	api.post('/:id', jwtCheck,
+		validate({
+			params: { id: Joi.number().integer().min(1).required() } ,
+			body: Joi.object().keys({
+				properties: Joi.object().required()
+			})
+		}),
+		(req, res, next) => {
+			contacts(config, db, logger).updateContact(req.params.id, req.body)
+			.then((data) => handleGeoResponse(data, req, res, next))
+				.catch((err) => {
+					/* istanbul ignore next */
+					logger.error(err);
+					/* istanbul ignore next */
+					next(err);
+				})
+		}
+	);
+
 	return api;
 };
