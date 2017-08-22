5e0e18320bd419720b07e330c64dbc25232efe87
Matthew Berryman <matthew@acrossthecloud.net>
2017-08-22 13:03:21 +1000

    work on #51

--- public/resources/js/contacts.js
+++ public/resources/js/contacts.js
@@ -5,11 +5,22 @@ var loadContacts = function(err, contacts){
   }
   else {
 
-    $('#contactsContainer').append('<table class="table table-striped" id="contactsTable"><thead><tr><th>Name</th><th>Org</th><th>Email</th><th>Cell</th></tr></thead><tbody>');
+    $('#contactsContainer').append('<table class="table table-striped" id="contactsTable"><thead><tr><th>Name</th><th>D.O.B.</th><th>Nationality 1</th><th>Nationality 2</th><th>Org</th><th>Email</th><th>Cell</th><th>WhatsApp</th><th>Twitter</th><th>Facebook</th></tr></thead><tbody>');
 
     $.each(contacts, function(key, value) {
       //console.log(key, value);
-      $('#contactsTable').append('<tr><td>'+value.properties.properties.name+'</td><td>'+value.properties.properties.type+'</td><td>'+value.properties.properties.email+'</td><td>'+value.properties.properties.cell+'</td></tr>');
+      $('#contactsTable').append('<tr><td>'+value.properties.properties.title + ' ' + value.properties.properties.name
+        +'</td><td>'+value.properties.properties.dob
+        +'</td><td>'+value.properties.properties.nationality1.name
+        +'</td><td>'+value.properties.properties.nationality2.name
+        +'</td><td>'+value.properties.properties.dob
+        +'</td><td>'+value.properties.properties.type
+        +'</td><td>'+value.properties.properties.email
+        +'</td><td>'+value.properties.properties.cell
+        +'</td><td>'+value.properties.properties.WhatsApp
+        +'</td><td>'+value.properties.properties.Twitter
+        +'</td><td>'+value.properties.properties.Facebook
+        +'</td></tr>');
     });
 
     $('#contactsTable').append('</tbody></table>');
