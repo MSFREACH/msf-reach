17285cae937e01b65f7f2bd71aefe43a0cc91c9e
Matthew Berryman <matthew@acrossthecloud.net>
2017-08-22 18:01:18 +1000

    fixes #76

--- public/resources/js/landing.js
+++ public/resources/js/landing.js
@@ -262,8 +262,8 @@ function onEachFeature(feature, layer) {
   var landingMap = L.map('landingMap').setView([-6.8, 108.7], 7);
 
   // Add some base tiles
-  var stamenTerrain = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}.{ext}', {
-    attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  var stamenTerrain = L.tileLayer('https://api.mapbox.com/styles/v1/clgeros/cj2lgdo3x000b2rmrv6iiii38/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiY2xnZXJvcyIsImEiOiJjajBodHJjdGYwM21sMndwNHk2cGxxajRnIn0.nrw2cFsVqjA2bclnKs-9mw', {
+    attribution: '© Mapbox © OpenStreetMap © DigitalGlobe',
     subdomains: 'abcd',
     minZoom: 0,
     maxZoom: 18,
@@ -271,7 +271,7 @@ function onEachFeature(feature, layer) {
   });
 
   // Add some satellite tiles
-  var mapboxSatellite = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoidG9tYXN1c2VyZ3JvdXAiLCJhIjoiY2o0cHBlM3lqMXpkdTJxcXN4bjV2aHl1aCJ9.AjzPLmfwY4MB4317m4GBNQ', {
+  var mapboxSatellite = L.tileLayer('https://api.mapbox.com/styles/v1/clgeros/cj2lds8kl00042smtdpniowm2/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiY2xnZXJvcyIsImEiOiJjajBodHJjdGYwM21sMndwNHk2cGxxajRnIn0.nrw2cFsVqjA2bclnKs-9mw', {
     attribution: '© Mapbox © OpenStreetMap © DigitalGlobe'
   }).addTo(landingMap);
 
