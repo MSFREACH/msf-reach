MSFREACH/msf-reach

commit 698e5298b52bd5165c334f79bf6716471e15246e

nomenclature tidy up for #76

public/resources/js/events.js
@@ -223,7 +223,7 @@ var mapboxSatellite = L.tileLayer('https://api.mapbox.com/styles/v1/clgeros/cj2l
 }).addTo(eventsMap);
 
 var baseMaps = {
-  "Terrain": stamenTerrain,
+  "Terrain": mapboxTerrain,
   "Satellite" : mapboxSatellite
 };
 

public/resources/js/landing.js
@@ -276,7 +276,7 @@ function onEachFeature(feature, layer) {
   }).addTo(landingMap);
 
   var baseMaps = {
-    "Terrain": stamenTerrain,
+    "Terrain": mapboxTerrain,
     "Satellite" : mapboxSatellite
   };
 

commit 0751e80b855c089680e5a2a35a53887515161bfa

698e5298b52bd5165c334f79bf6716471e15246e broke maps, this fixes

public/resources/js/events.js
@@ -313,7 +313,7 @@ if (currentEventId !== false && currentEventId != ''){
 var eventsMap = L.map('map').setView([-6.8, 108.7], 7);
 
 // Add some base tiles
-var stamenTerrain = L.tileLayer('https://api.mapbox.com/styles/v1/clgeros/cj2lgdo3x000b2rmrv6iiii38/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiY2xnZXJvcyIsImEiOiJjajBodHJjdGYwM21sMndwNHk2cGxxajRnIn0.nrw2cFsVqjA2bclnKs-9mw', {
+var mapboxTerrain = L.tileLayer('https://api.mapbox.com/styles/v1/clgeros/cj2lgdo3x000b2rmrv6iiii38/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiY2xnZXJvcyIsImEiOiJjajBodHJjdGYwM21sMndwNHk2cGxxajRnIn0.nrw2cFsVqjA2bclnKs-9mw', {
   attribution: '© Mapbox © OpenStreetMap © DigitalGlobe',
   subdomains: 'abcd',
   minZoom: 0,

public/resources/js/landing.js
@@ -262,7 +262,7 @@ function onEachFeature(feature, layer) {
   var landingMap = L.map('landingMap').setView([-6.8, 108.7], 7);
 
   // Add some base tiles
-  var stamenTerrain = L.tileLayer('https://api.mapbox.com/styles/v1/clgeros/cj2lgdo3x000b2rmrv6iiii38/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiY2xnZXJvcyIsImEiOiJjajBodHJjdGYwM21sMndwNHk2cGxxajRnIn0.nrw2cFsVqjA2bclnKs-9mw', {
+  var mapboxTerrain = L.tileLayer('https://api.mapbox.com/styles/v1/clgeros/cj2lgdo3x000b2rmrv6iiii38/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiY2xnZXJvcyIsImEiOiJjajBodHJjdGYwM21sMndwNHk2cGxxajRnIn0.nrw2cFsVqjA2bclnKs-9mw', {
     attribution: '© Mapbox © OpenStreetMap © DigitalGlobe',
     subdomains: 'abcd',
     minZoom: 0,