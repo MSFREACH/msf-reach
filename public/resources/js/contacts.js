d3f84706a35a520f08e931a9d2c5987b311bcc60
Matthew Berryman <matthew@acrossthecloud.net>
2017-08-23 11:17:34 +1000

    jshint suggested tidy up of contacts.js

--- public/resources/js/contacts.js
+++ public/resources/js/contacts.js
@@ -9,18 +9,18 @@ var loadContacts = function(err, contacts){
 
     $.each(contacts, function(key, value) {
       //console.log(key, value);
-      $('#contactsTable').append('<tr><td>'+(typeof(value.properties.properties.title)==='undefined' ? '' : value.properties.properties.title) + ' ' + value.properties.properties.name
-        +'</td><td>'+(typeof(value.properties.properties.email)==='undefined' ? '' : '<a href="mailto:'+value.properties.properties.email+'">'+value.properties.properties.email+'</a>')
-        +'</td><td>'+(typeof(value.properties.properties.cell)==='undefined' ? '' : value.properties.properties.cell)
-        +'</td><td>'+(typeof(value.properties.properties.type)==='undefined' ? '' : value.properties.properties.type)
-        +'</td><td>'+(typeof(value.properties.properties.dob)==='undefined' ? '' : value.properties.properties.dob)
-        +'</td><td>'+(typeof(value.properties.properties.nationality1)==='undefined' ? '' : value.properties.properties.nationality1.name)
-        +'</td><td>'+(typeof(value.properties.properties.nationality2)==='undefined' ? '' : value.properties.properties.nationality2.name)
-        +'</td><td>'+(typeof(value.properties.properties.WhatsApp)==='undefined' ? '' : value.properties.properties.WhatsApp)
-        +'</td><td>'+(typeof(value.properties.properties.Twitter)==='undefined' ? '' : value.properties.properties.Twitter)
-        +'</td><td>'+(typeof(value.properties.properties.Facebook)==='undefined' ? '' : value.properties.properties.Facebook)
-        +'</td><td>'+(typeof(value.properties.properties.Skype)==='undefined' ? '' : value.properties.properties.Skype)
-        +'</td></tr>');
+      $('#contactsTable').append('<tr><td>'+(typeof(value.properties.properties.title)==='undefined' ? '' : value.properties.properties.title) + ' ' + value.properties.properties.name +
+        '</td><td>'+(typeof(value.properties.properties.email)==='undefined' ? '' : '<a href="mailto:'+value.properties.properties.email+'">'+value.properties.properties.email+'</a>') +
+        '</td><td>'+(typeof(value.properties.properties.cell)==='undefined' ? '' : value.properties.properties.cell) + 
+        '</td><td>'+(typeof(value.properties.properties.type)==='undefined' ? '' : value.properties.properties.type) +
+        '</td><td>'+(typeof(value.properties.properties.dob)==='undefined' ? '' : value.properties.properties.dob) +
+        '</td><td>'+(typeof(value.properties.properties.nationality1)==='undefined' ? '' : value.properties.properties.nationality1.name) +
+        '</td><td>'+(typeof(value.properties.properties.nationality2)==='undefined' ? '' : value.properties.properties.nationality2.name) +
+        '</td><td>'+(typeof(value.properties.properties.WhatsApp)==='undefined' ? '' : value.properties.properties.WhatsApp) +
+        '</td><td>'+(typeof(value.properties.properties.Twitter)==='undefined' ? '' : value.properties.properties.Twitter) +
+        '</td><td>'+(typeof(value.properties.properties.Facebook)==='undefined' ? '' : value.properties.properties.Facebook) +
+        '</td><td>'+(typeof(value.properties.properties.Skype)==='undefined' ? '' : value.properties.properties.Skype) +
+        '</td></tr>');
     });
 
     $('#contactsTable').append('</tbody></table>');
