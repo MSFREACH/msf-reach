1d1881617a3bea0f961ab294a5fc934a203bccba
Matthew Berryman <matthew@acrossthecloud.net>
2017-08-23 14:05:20 +1000

    disable pre-filling event name for now cc @anexact re #30

--- public/resources/js/new-event.js
+++ public/resources/js/new-event.js
@@ -106,16 +106,19 @@ $(function(){
 
 	$("#inputEvDateTime").val((new Date()).toISOString());
 
+/*
 	$("#inputEvDateTime").change(function(){
 		$("#inputName").val($("#selectType".replace('_',' ')).val()+" "+$("#inputEvDateTime").val());
 	});
-
+*/
 	$('#selectType').change(function(){
 		$('#divNaturalDisaster').toggle(this.value == "natural_hazard");
 		$('#divDisease').toggle(this.value == "epidemiological");
 		$('#divOther').toggle(this.value == "other");
-		$("#inputName").val(this.value.replace('_',' ')+" "+$("#inputEvDateTime").val());
+		//$("#inputName").val(this.value.replace('_',' ')+" "+$("#inputEvDateTime").val());
 	});
+
+
   $('#inputDisasterType , #inputDiseaseType').change(function(){
 		console.log(this);
 		$('#divOther').toggle(!this.value);
