c3214f7fa66135c7fcb8f37c0c124c9131aad759
Matthew Berryman <matthew@acrossthecloud.net>
2017-08-23 11:10:56 +1000

    opacity for mission history icons also on events page #63

--- public/resources/js/events.js
+++ public/resources/js/events.js
@@ -287,6 +287,7 @@ var mapMissions = function(missions ){
     iconUrl: '/resources/images/icons/event_types/HISTORICAL-43.svg',
 
     iconSize:     [50, 50], // size of the icon
+    opacity: 0.8
     //iconAnchor:   [13, -13], // point of the icon which will correspond to marker's location
     //popupAnchor:  [13, 13] // point from which the popup should open relative to the iconAnchor
   });
