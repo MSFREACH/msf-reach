aebc753c1adb96fa2e82fec067b49ce9201dd37e
Raymund Delfin <raymund.delfin@hotmail.com>
2017-09-22 00:05:31 +0800

    work on #101

---
+++ src/lib/tsr.js
@@ -0,0 +1,65 @@
+import Promise from "bluebird";
+
+import cheerio from "cheerio";
+import { parseString } from "xml2js";
+import rp from "request-promise";
+import { parseDms } from "dms-conversion";
+
+const TSRMain = () =>
+  new Promise((resolve, reject) => {
+    let HOST = "http://www.tropicalstormrisk.com/tracker/dynamic";
+    var options = {
+      uri: `${HOST}/main_.html`,
+      transform: function(body) {
+        return cheerio.load(body);
+      }
+    };
+
+    rp(options)
+      .then(function($) {
+        let features = []; // store for features
+        
+        var tr = $('table.wide tbody tr');
+        if (tr.length >= 3) {
+          let updated = $(tr[1]).find('th').eq(0).text().trim();
+          for(var i = 3; i < tr.length; i++) {
+            let storm = $(tr[i]).find('td').eq(0).text().trim();
+            let basin = $(tr[i]).find('td').eq(1).text().trim();
+            let currentDataLat = $(tr[i]).find('td').eq(3).text().trim();
+            let currentDataLong = $(tr[i]).find('td').eq(4).text().trim();
+            let dmsStrings = [currentDataLat, currentDataLong];
+            let currentDataCoords = dmsStrings.map(parseDms);
+            let currentDataWind = $(tr[i]).find('td').eq(5).text().trim();
+            let currentDataCat = $(tr[i]).find('td').eq(6).text().trim();
+            
+            // define a feature
+            let feature = {
+              type: "Feature",
+              geometry: { type: "Point", coordinates: [] },
+              properties: {}
+            };
+            // extract coords
+            feature.geometry.coordinates.push(currentDataCoords[0]);
+            feature.geometry.coordinates.push(currentDataCoords[1]);
+            // extract properties
+            feature.properties["title"] = "Storm - " + storm + " in " + basin;
+            let url = ($(tr[i]).find('td').eq(0).find('a').attr('href')).split('./');
+            feature.properties["link"] = HOST + '/' + url[1];
+            feature.properties["id"] = url[1].split('.')[0];
+            feature.properties["updated"] = (new Date(updated)).toISOString();
+            feature.properties["summary"] = "Wind: " + currentDataWind + " Category: " + currentDataCat;
+
+            // push feature to feature collection
+            features.push(feature);          
+          }
+        }
+       
+        // return GeoJSON
+        resolve({ type: "FeatureCollection", features: features });
+      })
+      .catch(function(err) {
+        console.log("Error", err);
+      });
+  });
+
+module.exports = { TSRMain };
