8609f78c1c14d0923ac8c7cea683da6e43f73898
Raymund Delfin <raymund.delfin@hotmail.com>
2017-09-23 22:31:34 +0800

    work on #104 as fields available in New Contact page

--- public/resources/js/contacts.js
+++ public/resources/js/contacts.js
@@ -1,23 +1,49 @@
 // Load Contacts to contacts table tab
-var loadContacts = function(err, contacts){
-  if (err){
-    alert('Error loading contacts: '+ err);
-  }
-  else {
-
-    $('#contactsContainer').html('<table class="table table-striped" id="contactsTable"><thead><tr><th>Name</th><th>Email</th><th>Mobile</th><th>Speciality</th></tr></thead><tbody>');
+var loadContacts = function(err, contacts) {
+  if (err) {
+    alert("Error loading contacts: " + err);
+  } else {
+    $("#contactsContainer").html(
+      '<table class="table table-striped" id="contactsTable"><thead><tr><th>&nbsp;</th><th>Name</th><th>Email</th><th>Mobile</th><th>Speciality</th></tr></thead><tbody>'
+    );
 
     $.each(contacts, function(key, value) {
-      //console.log(key, value);
-      $('#contactsTable').append('<tr><td>'+(typeof(value.properties.properties.title)==='undefined' ? '' : value.properties.properties.title) + ' ' + value.properties.properties.name +
-        '</td><td class="emailcell">'+(typeof(value.properties.properties.email)==='undefined' ? '' : '<a href="mailto:'+value.properties.properties.email+'">'+value.properties.properties.email+'</a>') +
-        '</td><td>'+(typeof(value.properties.properties.cell)==='undefined' ? '' : value.properties.properties.cell) +
-        '</td><td>'+(typeof(value.properties.properties.speciality)==='undefined' ? '' : value.properties.properties.speciality) +
-        '</td></tr>');
+      // console.log(key, value);
+      $("#contactsTable").append(
+        "<tr><td><a href='#' onclick='onContactLinkClick(" +
+          value.properties.id +
+          ")' class='contact-link btn btn-sm btn-primary' title='Quick View'><i class='glyphicon glyphicon-eye-open'></i></a></td><td>" +
+          (typeof value.properties.properties.title === "undefined"
+            ? ""
+            : value.properties.properties.title) +
+          " " +
+          value.properties.properties.name +
+          '</td><td class="emailcell">' +
+          (typeof value.properties.properties.email === "undefined"
+            ? ""
+            : '<a href="mailto:' +
+              value.properties.properties.email +
+              '">' +
+              value.properties.properties.email +
+              "</a>") +
+          "</td><td>" +
+          (typeof value.properties.properties.cell === "undefined"
+            ? ""
+            : value.properties.properties.cell) +
+          "</td><td>" +
+          (typeof value.properties.properties.speciality === "undefined"
+            ? ""
+            : value.properties.properties.speciality) +
+          "</td></tr>"
+      );
     });
 
-    $('#contactsTable').append('</tbody></table>');
+    $("#contactsTable").append("</tbody></table>");
   }
+};
+
+var onContactLinkClick = function(id) {
+  window.open('/contact/details?id=' + id, "popupWindow", "width=600,height=640,scrollbars=auto");
 };
 
 // Perform GET call to get tweets
