4d31275807bf2f421dd2e71b51533e33565b552f
Raymund Delfin <raymund.delfin@hotmail.com>
2017-09-28 01:43:37 +0800

    partial work on #106

--- public/resources/js/missions.js
+++ public/resources/js/missions.js
@@ -1,17 +1,33 @@
 // Load Missions to missions table tab
 var loadMissions = function(err, missions) {
-  if (err){
-    alert('Error loading missions: '+ err);
-  }
-  else {
-
-    $('#missionsContainer').html('<table class="table table-striped" id="missionsTable"><thead><tr><th>Name</th><th>Region</th><th>Start</th><th>End</th><th>Severity</th><th>Capacity</th></tr></thead><tbody>');
+  if (err) {
+    alert("Error loading missions: " + err);
+  } else {
+    $("#missionsContainer").html(
+      '<table class="table table-striped" id="missionsTable"><thead><tr><th>&nbsp;</th><th>Name</th><th>Region</th><th>Start</th><th>End</th><th>Severity</th><th>Capacity</th></tr></thead><tbody>'
+    );
 
     $.each(missions, function(key, value) {
-      $('#missionsTable').append('<tr><td>'+value.properties.properties.name+'</td><td>'+value.properties.properties.region+'</td><td>'+value.properties.properties.startDate+'</td><td>'+value.properties.properties.finishDate+'</td><td>'+value.properties.properties.severity+'</td><td>'+value.properties.properties.capacity+'</td></tr>');
+      $("#missionsTable").append(
+        "<tr><td><a data-toggle='modal' data-target='#missionModal' href='#' onclick='onMissionLinkClick(" +
+          value.properties.id +
+          ")' class='contact-link btn btn-sm btn-primary' title='Quick View'><i class='glyphicon glyphicon-eye-open'></i></a></td><td>" +
+          value.properties.properties.name +
+          "</td><td>" +
+          value.properties.properties.region +
+          "</td><td>" +
+          value.properties.properties.startDate +
+          "</td><td>" +
+          value.properties.properties.finishDate +
+          "</td><td>" +
+          value.properties.properties.severity +
+          "</td><td>" +
+          value.properties.properties.capacity +
+          "</td></tr>"
+      );
     });
 
-    $('#missionsTable').append('</tbody></table>');
+    $("#missionsTable").append("</tbody></table>");
   }
 };
 
@@ -33,3 +49,27 @@ $("#messSearchTerm").on("input", function() {
   var throttFunc=_.throttle(getMissions, 300);
   throttFunc(this.value);
 });
+
+var missionData = {};
+var onMissionLinkClick = function(id) {
+  async.waterfall([
+    function(callback) {
+      // Load Mission Details template to BT Modal 1st
+      $( "#missionModalBody" ).load( "/resources/tpl/missions/details.html" );
+      callback();
+    },
+    function(callback) {
+      $.getJSON("/api/missions/" + id, function(data) {
+        missionData = data.result ? data.result.properties : {};
+        _(missionData).forIn(function(value, key) {
+          console.log("Key:", key, "Value:", value);
+          // if (key === "nationality1" || key === "nationality2") {
+          //   value = value.name;
+          // }
+          $("span.event-" + key).html(value);
+        });
+        callback();
+      });      
+    }
+  ]);
+};
--- src/api/routes/missions/index.js
+++ src/api/routes/missions/index.js
@@ -14,24 +14,43 @@ import validate from 'celebrate';
 import shortid from 'shortid';
 
 export default ({ config, db, logger }) => {
-	let api = Router();
+    let api = Router();
 
-	api.get('/', jwtCheck, cacheResponse('10 minutes'),
-		validate({
-			query: {
-				search: Joi.string().min(1),
-				geoformat: Joi.any().valid(config.GEO_FORMATS).default(config.GEO_FORMAT_DEFAULT)
-			}
-		}),
-		(req, res, next) => missions(config, db, logger).all(req.query.search)
-			.then((data) => handleGeoResponse(data, req, res, next))
-			.catch((err) => {
-				/* istanbul ignore next */
-				logger.error(err);
-				/* istanbul ignore next */
-				next(err);
-			})
-	);
+    api.get('/', jwtCheck, cacheResponse('10 minutes'),
+        validate({
+            query: {
+                search: Joi.string().min(1),
+                geoformat: Joi.any().valid(config.GEO_FORMATS).default(config.GEO_FORMAT_DEFAULT)
+            }
+        }),
+        (req, res, next) => missions(config, db, logger).all(req.query.search)
+            .then((data) => handleGeoResponse(data, req, res, next))
+            .catch((err) => {
+                /* istanbul ignore next */
+                logger.error(err);
+                /* istanbul ignore next */
+                next(err);
+            })
+    );
 
-	return api;
+    api.get('/:id',jwtCheck, cacheResponse('1 minute'),
+        validate({
+            params: { id: Joi.number().integer().min(1).required() } ,
+            query: {
+                geoformat: Joi.any().valid(config.GEO_FORMATS).default(config.GEO_FORMAT_DEFAULT)
+            }
+        }),
+        (req, res, next) => missions(config, db, logger).byId(req.params.id)
+            .then((data) => {
+                res.status(200).json({ statusCode: 200, time:new Date().toISOString(), result: data });
+            })
+            .catch((err) => {
+                /* istanbul ignore next */
+                logger.error(err);
+                /* istanbul ignore next */
+                next(err);
+            })
+    );
+
+    return api;
 };
