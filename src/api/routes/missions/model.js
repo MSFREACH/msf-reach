9ccf6fe2bf57b868fdaebfa094217cce77f92c9b
Raymund Delfin <raymund.delfin@hotmail.com>
2017-09-28 01:43:37 +0800

    partial work on #106

--- src/api/routes/missions/model.js
+++ src/api/routes/missions/model.js
@@ -30,5 +30,27 @@ export default (config, db, logger) => ({
 		db.any(query, values).timeout(config.PGTIMEOUT)
 			.then((data) => resolve(data))
 			.catch((err) => reject(err));
-	})
+	}),
+
+  /**
+   * Return mission specified by ID
+   * @param {integer} id ID of mission
+   */
+  byId: id =>
+    new Promise((resolve, reject) => {
+      // Setup query
+      let query = `SELECT id, properties, the_geom
+        FROM ${config.TABLE_MISSIONS}
+        WHERE id = $1`;
+
+      // Setup value{s}
+      let values = [id];
+
+      // Execute
+      logger.debug(query, values);
+      db.oneOrNone(query, values)
+        .timeout(config.PGTIMEOUT)
+        .then(data => resolve(data))
+        .catch(err => reject(err));
+    })
 });
