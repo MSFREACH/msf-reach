84ee646d63162e235bf02842ccb096d7df74fd9c
Mehrdad Amirghasemi <mehrdad.amirghasemi@gmail.com>
2017-10-22 16:23:24 +1100

    a global function for #144

--- public/resources/js/nav.js
+++ public/resources/js/nav.js
@@ -6,3 +6,12 @@ $('#logout').click(function() {
   Cookies.remove('jwt', {path: '/'} );
   localStorage.removeItem("username");
 });
+
+var handleUnAuthorized= function (errorObject)
+{
+  if (errorObject.responseJSON.status == 401)
+   {
+     alert("It seems you have been logged out of the system. You will be redirected to login page.");
+     location.href="/";
+   }
+}
