f81402c910163cdb79f5e428b13fc60a6b6ea4fb
Raymund Delfin <raymund.delfin@hotmail.com>
2017-10-22 22:07:36 +0800

    work on #144 - monitor 401 status code & redirect to login page

---
+++ public/resources/js/common.js
@@ -0,0 +1,14 @@
+/**
+ * Monitor status code to redirect login page if session expires.
+*/
+$(function() {
+	$.ajaxSetup({
+		statusCode: {
+			401: function() {
+				if (window.location.pathname !== "/login/") {
+					window.location.href = "/login/";
+				}
+			}
+		}
+	});
+});
