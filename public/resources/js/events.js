MSFREACH/msf-reach

commit 13e1b6ebb1d6a6832cb68d78698749076b7d3126

backwards compatible display of event_datetime on landing page map and table for #146, #107

public/resources/js/events.js
@@ -300,7 +300,7 @@ var mapReports = function(reports){
       popupContent += 'Decription: '+ feature.properties.content.description + '<BR>';
       popupContent += 'Tag: '+ feature.properties.content.report_tag + '<BR>';
       popupContent += 'Reporter: ' + feature.properties.content["username/alias"] + '<BR>';
-      popupContent += 'Created: ' + feature.properties.created + '<BR>';
+      popupContent += 'Opened: ' + feature.properties.event_datetime + '<BR>';
       if (feature.properties.content.image_link && feature.properties.content.image_link.length > 0){
         popupContent += '<img src="'+feature.properties.content.image_link+'" height="140">';
       }

public/resources/js/landing.js
@@ -78,7 +78,7 @@ var mapAllEvents = function(err, events){
     "'><img src='/resources/images/icons/event_types/"+icon_name+".svg' width='40'></a>" +
     "<strong><a href='/events/?eventId=" + feature.properties.id +
     "'>" + feature.properties.metadata.name +"</a></strong>" + "<BR>" +
-    "Created: " + feature.properties.created + "<BR>" +
+    "Opened: " + (feature.properties.metadata.event_datetime || feature.properties.created) + "<BR>" +
     "Type: " + type.replace('_',' ') + "<br>" +
     statusStr +
     notificationStr +
@@ -93,7 +93,7 @@ var mapAllEvents = function(err, events){
     $('#eventProperties').append(
       '<div class="list-group-item">' +
       'Name: <a href="/events/?eventId=' + feature.properties.id + '">' + feature.properties.metadata.name + '</a><br>' +
-      'Created: ' + feature.properties.created + '<br>' +
+      'Opened: ' + (feature.properties.metadata.event_datetime || feature.properties.created) + '<br>' +
       'Type: ' + feature.properties.type + '<br>' +
       statusStr +
       notificationStr +

commit b0b96916e3e950052a4d1cc9167330249b73d353

fix report display bug introduced in 13e1b6ebb1d6a6832cb68d78698749076b7d3126

public/resources/js/events.js
@@ -300,7 +300,7 @@ var mapReports = function(reports){
       popupContent += 'Decription: '+ feature.properties.content.description + '<BR>';
       popupContent += 'Tag: '+ feature.properties.content.report_tag + '<BR>';
       popupContent += 'Reporter: ' + feature.properties.content["username/alias"] + '<BR>';
-      popupContent += 'Opened: ' + feature.properties.event_datetime + '<BR>';
+      popupContent += 'Reported time: ' + feature.properties.created + '<BR>';
       if (feature.properties.content.image_link && feature.properties.content.image_link.length > 0){
         popupContent += '<img src="'+feature.properties.content.image_link+'" height="140">';
       }