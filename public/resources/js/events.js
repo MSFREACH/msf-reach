cfbcef2216cccb964ea32023b961c5644c29e7c3
Matthew Berryman <matthew@acrossthecloud.net>
2018-02-15 14:11:38 +1100

    order notifications by most recent -> least recent #251

--- public/resources/js/events.js
+++ public/resources/js/events.js
@@ -274,7 +274,7 @@ var printEventProperties = function(err, eventProperties){
 
         if (typeof(eventProperties.metadata.notification) !== 'undefined' && eventProperties.metadata.notification.length > 0 ) {
 
-          $('#eventNotifications').append('<table class="table"><thead><tr><td>Time</td><td>Notification</td></tr></thead><tbody>'+eventProperties.metadata.notification.reduce(reduceNotificationArray,'')+'</tbody></table>');
+          $('#eventNotifications').append('<table class="table"><thead><tr><td>Time</td><td>Notification</td></tr></thead><tbody>'+eventProperties.metadata.notification.reduceRight(reduceNotificationArray,'')+'</tbody></table>');
 
         }
         var extra_metadata = unpackMetadata(eventProperties.metadata);
