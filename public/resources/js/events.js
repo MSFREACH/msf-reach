29d508cf3e91e82889bd2dd92e56463ab00f6fa5
Matthew Berryman <matthew@acrossthecloud.net>
2018-03-02 18:12:26 +1100

    eslint indentation fixes

--- public/resources/js/events.js
+++ public/resources/js/events.js
@@ -190,24 +190,24 @@ var printEventProperties = function(err, eventProperties){
         var searchTerm = '';
         if (currentEventProperties) {
             if (currentEventProperties.metadata.name) {
-              if (currentEventProperties.metadata.name.includes('_')) {
-                  elements = currentEventProperties.metadata.name.split('_');
-                  for (var i = 0; i < elements.length-1; i++) {
-                      searchTerm += elements[i] + ' ';
-                  }
-              } else {
-                  searchTerm = currentEventProperties.metadata.name;
-              }
+                if (currentEventProperties.metadata.name.includes('_')) {
+                    elements = currentEventProperties.metadata.name.split('_');
+                    for (var i = 0; i < elements.length-1; i++) {
+                        searchTerm += elements[i] + ' ';
+                    }
+                } else {
+                    searchTerm = currentEventProperties.metadata.name;
+                }
             } else {
-              if (currentEventProperties.hasOwnProperty('type')) {
-                searchTerm = currentEventProperties.type.replace(',','');
-              }
-              if (currentEventProperties.hasOwnProperty('sub_type')) {
-                searchTerm = currentEventProperties.metadata.sub_type.replace(',','');
-              }
-              if (currentEventProperties.metadata.hasOwnProperty('event_datetime')) {
-                searchTerm += ' ' + currentEventProperties.metadata.event_datetime;
-              }
+                if (currentEventProperties.hasOwnProperty('type')) {
+                    searchTerm = currentEventProperties.type.replace(',','');
+                }
+                if (currentEventProperties.hasOwnProperty('sub_type')) {
+                    searchTerm = currentEventProperties.metadata.sub_type.replace(',','');
+                }
+                if (currentEventProperties.metadata.hasOwnProperty('event_datetime')) {
+                    searchTerm += ' ' + currentEventProperties.metadata.event_datetime;
+                }
             }
             if (currentEventProperties.metadata.hasOwnProperty('country')) {
                 searchTerm += currentEventProperties.metadata.country;
