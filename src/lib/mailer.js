1104d453b0bd6b1a38003ae73db129b144d7a4b5
Tomas <tomasholderness@gmail.com>
2018-03-08 15:17:31 -0500

    fix up syntax errors

--- src/lib/mailer.js
+++ src/lib/mailer.js
@@ -6,39 +6,39 @@ import nodemailer from 'nodemailer';
 
 export default ( config, logger ) => ({
 
-  send: (recipient, emContext) => new Promise((resolve, reject) => {
+    send: (recipient, emContext) => new Promise((resolve, reject) => {
 
-    const smtpConfig = {
-        host: 'email-smtp.us-west-2.amazonaws.com',
-        port: 465,
-        secure: true,
-        requireTLS: true,
-        auth: {
-            user: config.SMTP_USER,
-            pass: config.SMTP_PASS
-        }
-    };
+        const smtpConfig = {
+            host: 'email-smtp.us-west-2.amazonaws.com',
+            port: 465,
+            secure: true,
+            requireTLS: true,
+            auth: {
+                user: config.SMTP_USER,
+                pass: config.SMTP_PASS
+            }
+        };
 
-    const transport = nodemailer.createTransport(smtpConfig);
+        const transport = nodemailer.createTransport(smtpConfig);
 
-    const mailOptions = {
-        from: 'MSF-REACH <admin@msf-reach.org>', // sender address
-        to: recipient,
-        subject: 'Update your MSF-REACH contact details',
-        template: 'fixme',
-        context: emContext
-    };
+        const mailOptions = {
+            from: 'MSF-REACH <admin@msf-reach.org>', // sender address
+            to: recipient,
+            subject: 'Update your MSF-REACH contact details',
+            template: 'fixme',
+            context: emContext
+        };
 
-    // send mail with defined transport object
-    logger.info('Sending email to ' + recipient);
-    transport.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            logger.error(error.message);
-            reject(error);
-        }
-        else
-            logger.info('Email %s sent: %s', info.messageId, info.response);
+        // send mail with defined transport object
+        logger.info('Sending email to ' + recipient);
+        transport.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                logger.error(error.message);
+                reject(error);
+            }
+            else
+                logger.info('Email %s sent: %s', info.messageId, info.response);
             resolve(info); // fixme probably want to resolve something useful
-    });
-  })
+        });
+    })
 });
