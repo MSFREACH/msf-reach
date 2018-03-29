0373bc50a28527c20ff61162ba647fc5aea36eed
stefan-devika <stefan@devika.technology>
2018-03-29 14:43:47 +1100

    #79 rebuild

--- src/lib/util.js
+++ src/lib/util.js
@@ -54,11 +54,10 @@ const ensureAuthenticated = (req, res, next) => {
             return
         }
         */
-
-        if (req.user.groups.indexOf(config.AZURE_AD_OPERATORS_GROUP_ID) > -1) && req.isAuthenticated()) {  //since express-jwt is "Middleware that validates JsonWebTokens and sets req.user." this should work.
-            return next();
-        }
         
+        if (req.user.groups.indexOf(config.AZURE_AD_OPERATORS_GROUP_ID) > -1) && req.isAuthenticated()) {  //since express-jwt is "Middleware that validates JsonWebTokens and sets req.user." this should work.
+        return next();
+        }
         res.redirect('/login');
         return;
     });
