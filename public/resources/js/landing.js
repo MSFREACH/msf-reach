32cf9e18aee3d1df3ff1a4e6598ad978464d0459
Matthew Berryman <matthew@acrossthecloud.net>
2018-04-16 10:35:24 +0800

    issue #372 bring search into tab issue #372 make monitoring & assessment the default

--- public/resources/js/landing.js
+++ public/resources/js/landing.js
@@ -710,9 +710,6 @@ $('#landing_tabs').on('click', 'a[data-toggle="tab"]', function(e) {
         //remove active class from other tab-panes
         $('.tab-content:not(.' + $link.attr('href').replace('#','') + ') .tab-pane').removeClass('active');
 
-        // click first submenu tab for active section
-        $('a[href="' + $link.attr('href') + '_event"][data-toggle="tab"]').click();
-
         // activate tab-pane for active section
         $('.tab-content.' + $link.attr('href').replace('#','') + ' .tab-pane:first').addClass('active');
       }
