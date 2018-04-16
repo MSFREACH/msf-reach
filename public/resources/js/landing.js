b77e4f4f844ec60825c1e83e71080263e851aaee
Matthew Berryman <matthew@acrossthecloud.net>
2018-04-16 10:14:28 +0800

    basic tabs in place, js working #372

--- public/resources/js/landing.js
+++ public/resources/js/landing.js
@@ -699,3 +699,22 @@ mainMap.on('overlayremove', function (layersControlEvent) {
         Cookies.set(layersControlEvent.name,'off');
     }
 });
+
+$('#landing_tabs').on('click', 'a[data-toggle="tab"]', function(e) {
+      e.preventDefault();
+
+      var $link = $(this);
+
+      if (!$link.parent().hasClass('active')) {
+
+        //remove active class from other tab-panes
+        $('.tab-content:not(.' + $link.attr('href').replace('#','') + ') .tab-pane').removeClass('active');
+
+        // click first submenu tab for active section
+        $('a[href="' + $link.attr('href') + '_event"][data-toggle="tab"]').click();
+
+        // activate tab-pane for active section
+        $('.tab-content.' + $link.attr('href').replace('#','') + ' .tab-pane:first').addClass('active');
+      }
+
+});
