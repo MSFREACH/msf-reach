923c6f73a1ba35de094e31fd6d7aa5cda968eac2
Matthew Berryman <matthew@acrossthecloud.net>
2018-04-17 11:56:44 +0800

    also check against email2

--- src/api/routes/contacts/update/model.js
+++ src/api/routes/contacts/update/model.js
@@ -8,7 +8,7 @@ import Promise from 'bluebird';
 export default (config, db, logger) => ({
 
     requestGUID: (email) => new Promise((resolve, reject) => {
-        let query = `UPDATE ${config.TABLE_CONTACTS} SET properties = properties ||('{"guid":"'||gen_random_uuid()||'", "guid_timestamp":"'||extract (epoch from now())||'"}')::jsonb WHERE properties->>'email' = $1 RETURNING properties->>'guid' AS guid`;
+        let query = `UPDATE ${config.TABLE_CONTACTS} SET properties = properties ||('{"guid":"'||gen_random_uuid()||'", "guid_timestamp":"'||extract (epoch from now())||'"}')::jsonb WHERE (properties->>'email' = $1 OR properties ->>'email2' = $1) RETURNING properties->>'guid' AS guid`;
 
         let values = [ email ];
 
@@ -20,7 +20,7 @@ export default (config, db, logger) => ({
     }),
 
     validateGUID: (email, guid) => new Promise((resolve, reject) => {
-        let query = `SELECT id FROM ${config.TABLE_CONTACTS} WHERE properties->>'email' = $1 AND properties->>'guid' = $2 AND (extract(epoch from now()) - (properties->>'guid_timestamp')::real) <= ${config.PEER_GUID_TIMEOUT}`;
+        let query = `SELECT id FROM ${config.TABLE_CONTACTS} WHERE (properties->>'email' = $1 or properties->>'email2' = $2) AND properties->>'guid' = $2 AND (extract(epoch from now()) - (properties->>'guid_timestamp')::real) <= ${config.PEER_GUID_TIMEOUT}`;
 
         let values = [email, guid];
 
