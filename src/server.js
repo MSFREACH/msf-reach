f8ff447d3a8fb4e39a5fd2f05b59dadd92d65d4a
Matthew Berryman <matthew@acrossthecloud.net>
2018-04-20 17:14:04 +0800

    missing secret #332

--- src/server.js
+++ src/server.js
@@ -52,9 +52,10 @@ const init = (config, initializeDb, routes, logger) => new Promise((resolve, rej
         const authenticationStrategy = new OIDCStrategy({
             identityMetadata: `https://login.microsoftonline.com/${config.AZURE_AD_TENANT_NAME}.onmicrosoft.com/.well-known/openid-configuration`,
             clientID: config.AZURE_AD_CLIENT_ID,
+            clientSecret: config.SESSION_SECRET,
             redirectUrl: config.AZURE_AD_RETURN_URL,
             allowHttpForRedirectUrl: !config.REDIRECT_HTTP,
-            responseType: 'id_token+code', //For openID Connect auth. See: https://docs.microsoft.com/en-us/azure/active-directory/develop/active-directory-protocols-openid-connect-code
+            responseType: 'id_token code', //For openID Connect auth. See: https://docs.microsoft.com/en-us/azure/active-directory/develop/active-directory-protocols-openid-connect-code
             responseMode: 'form_post' //This is recommended by MS
         },
         function(iss, sub, profile, jwtClaims, access_token, refresh_token, params, done){
@@ -160,8 +161,8 @@ const init = (config, initializeDb, routes, logger) => new Promise((resolve, rej
                                 grant_type:'authorization_code',
                                 client_id: config.AZURE_AD_CLIENT_ID,
                                 resource:'https://graph.windows.net',
-                                client_secret:config.SESSION_SECRET,
-                                code:req.body.code,
+                                client_secret: config.SESSION_SECRET,
+                                code: req.body.code,
                                 redirect_uri: config.AZURE_AD_RETURN_URL
                             }
                         };
