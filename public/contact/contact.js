dd65efbe114385ca0e662a8f6c843e41e10002da
Matthew Berryman <matthew@acrossthecloud.net>
2018-05-24 19:29:31 +1000

    skip opening page if GUID and email

--- public/contact/contact.js
+++ public/contact/contact.js
@@ -20,6 +20,7 @@ $(function () {
 
     if (qGUID && qEmail)
     {
+        navigateTo(2);
         updateMode=true;
         $('#updateModal').modal('show');
         $('#updateModalMsg').html('Loading contact data ...');
