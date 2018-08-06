a5bc17e24bcdeecabf8a0756977db65975a23cde
Qiao Lin <qiaoclin@gmail.com>
2018-08-06 16:56:17 +0200

    #569 scrollTobottom getTweet again with lastID

--- public/resources/js/twitter.js
+++ public/resources/js/twitter.js
@@ -2,24 +2,39 @@
 var tweetIdHTMLMap = {}; // Dictionary of tweetID + embedded tweet HTML markup
 
 // Load Tweets to page
-var loadTweets = function(err, tweets) {
+var loadTweets = function(err, tweets, next) {
     if (err){
         alert('Error loading tweets: '+ err);
     }
     else {
-        $('#tweetFeed').empty();
+        if(!next){
+          $('#tweetFeed').empty(); // here if same searchString then we just append
+        }
         $.each(tweets, function(key, value){
             $('#tweetFeed').append('<div id="'+value.tweetId+'" draggable="true" ondragstart="drag(event)">'+value.tweetEmbed.html+'</div>');
             tweetIdHTMLMap[value.tweetId] = value.tweetEmbed.html;
         });
+
+
         twttr.widgets.load();
+
+        // $('#tweets').scroll(checkScroll);
+
     }
 };
 
+var max_id = '';
 // Perform GET call to get tweets
-var getTweets = function(searchString) { // eslint-disable-line no-unused-vars
-    $.getJSON('/api/twitter/?searchString=' + searchString, function (data){
-        loadTweets(null, data.result);
+var getTweets = function(searchString, next) { // eslint-disable-line no-unused-vars
+  var url = '/api/twitter/?searchString=' + searchString
+  if(next){
+    url += ('&max_id=' + max_id)
+  }
+    $.getJSON(url, function (data){
+
+        max_id = data.lastId;
+        console.log(url, data, data.result, max_id)
+        loadTweets(null, data.result, next);
     }).fail(function(err) {
         if (err.responseText.includes('expired')) {
             alert('session expired');
@@ -28,3 +43,19 @@ var getTweets = function(searchString) { // eslint-disable-line no-unused-vars
         }
     });
 };
+//
+// $(document).ready(function(){
+//
+//   $( window ).scroll(function() {
+//     console.log(" scroooolll ",)
+//   });
+// });
+
+function checkScroll(e){
+  console.log('scrooollllll ---- ', e)
+
+  var elem = $(e.currentTarget);
+  if(elem[0].scrollHeight - elem.scrollTop() == elem.outerHeight()){
+    console.log('here at the botttom of tweets')
+  }
+}
--- src/api/routes/twitter/index.js
+++ src/api/routes/twitter/index.js
@@ -18,17 +18,25 @@ export default ({ logger }) => {
     api.get('/', ensureAuthenticated, cacheResponse('1 minute'),
         validate({
             query: {
-                searchString: Joi.string().required()
+                searchString: Joi.string().required(),
+                max_id: Joi.string().allow('') /// current id string
             }
         }),
-        (req, res, next) => searchTwitter(req.query.searchString)
+        (req, res, next) => searchTwitter(req.query.searchString, req.query.id)
             .then((data) => {
                 let promiseArray = [];
+                /// here should get a new id string
+                if(data.statuses && data.statuses.length > 0){
+                  var lastId = data.statuses[data.statuses.length-1].id // also the oldest one, so working backwards
+                  console.log('routes/twitter/index.js ------ new IDStr____ ',lastId)
+
+                }
+
                 for (var i = 0; i < data.statuses.length; i++) {
                     promiseArray.push(embedTweet(data.statuses[i].id_str, 'https://twitter.com/'+data.statuses[i].user.screen_name+'/status/'+data.statuses[i].id_str));
                 }
                 Promise.all(promiseArray).then(function(resultArray) {
-                    res.status(200).json({statusCode: 200, result:resultArray});
+                    res.status(200).json({statusCode: 200, result:resultArray, lastId: lastId});
                 });
                 //.then((result) => res.status(200).json({ statusCode: 200, result: result }))
 
