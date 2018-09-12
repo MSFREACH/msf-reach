0e1888512fba3c13ff16de512f7fe629ce6e665c
Matthew Berryman <matthew@acrossthecloud.net>
2018-09-12 12:52:11 +1000

    fix bugs by fixing unit tests

--- src/api/routes/events/model.js
+++ src/api/routes/events/model.js
@@ -68,7 +68,7 @@ export default (config, db, logger) => ({
         // Setup query
         let queryOne = `INSERT INTO ${config.TABLE_EVENTS}
 			(status, type, created_at, updated_at, metadata, the_geom, subscribers)
-			VALUES ($1, $2, $3, now(), $4, ST_SetSRID(ST_Point($5,$6),4326), (select array_to_json(array(select distinct (subscribers ||  jsonb_build_array($7))))))
+			VALUES ($1, $2, $3, now(), $4, ST_SetSRID(ST_Point($5,$6),4326),jsonb_build_array($7))
 			RETURNING id, report_key, the_geom`;
         let queryTwo = `UPDATE ${config.TABLE_REPORTS}
       SET event_id=$1,report_key=(SELECT report_key from ${config.TABLE_EVENTS} WHERE id=$1),status='unconfirmed' where id=$2
@@ -152,7 +152,7 @@ export default (config, db, logger) => ({
       updated_at = now(),
             type = $4,
 			metadata = metadata || $2,
-      subscribers = (select array_to_json(array(select distinct (subscribers ||  jsonb_build_array($7)))))
+      subscribers = (select array_to_json(array(select distinct (subscribers ||  jsonb_build_array($5)))))
 			WHERE id = $3
 			RETURNING type, created_at, updated_at, report_key, metadata, ST_X(the_geom) as lng, ST_Y(the_geom) as lat`;
 
