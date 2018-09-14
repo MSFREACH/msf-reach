1099a26a502464b368bdd150a15e27e79e03a75c
Matthew Berryman <matthew@acrossthecloud.net>
2018-09-14 13:28:12 +1000

    eslint

--- src/api/routes/events/model.js
+++ src/api/routes/events/model.js
@@ -184,7 +184,7 @@ export default (config, db, logger) => ({
         // Execute
         logger.debug(query);
         db.one(query, values).timeout(config.PGTIMEOUT)
-            .then((data) => resolve({ id: String(id) }))
+            .then((data) => resolve({ id: String(id) })) // eslint-disable-line no-unused-vars
             .catch((err) => reject(err));
     }),
 
