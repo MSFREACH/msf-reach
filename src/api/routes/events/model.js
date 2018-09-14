1f7f523e9f379dac3c527b7a8568a4b0af722099
Matthew Berryman <matthew@acrossthecloud.net>
2018-09-14 13:07:10 +1000

    fix subscription response

--- src/api/routes/events/model.js
+++ src/api/routes/events/model.js
@@ -184,7 +184,7 @@ export default (config, db, logger) => ({
         // Execute
         logger.debug(query);
         db.one(query, values).timeout(config.PGTIMEOUT)
-            .then((data) => resolve({ id: String(id), status: body.status, type:data.type, created: data.created, reportkey:data.report_key, metadata:data.metadata, lat: data.lat, lng: data.lng }))
+            .then((data) => resolve({ id: String(id) }))
             .catch((err) => reject(err));
     }),
 
