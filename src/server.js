8384fb15fa857abb65392e867cc4628641918a5b
Matthew Berryman <matthew@acrossthecloud.net>
2018-10-30 15:42:42 +1100

    #174 further tweak to pick up user email for to field

--- src/server.js
+++ src/server.js
@@ -170,7 +170,7 @@ const init = (config, initializeDb, routes, logger) => new Promise((resolve, rej
 
                         //set a cookie here and then on the static page store it in localstorage
                         res.cookie('userdisplayName', req.user.displayName, { maxAge: 1000 * 60 * 1 }); //1 min cookie age should be enough
-                        res.cookie('email', req.user.preferred_username);
+                        res.cookie('email', req.user._json.preferred_username);
                         res.cookie('oid', req.user.oid, { maxAge: 1000 * 60 * 1 });
                         res.redirect('/authreturn');
                     });
