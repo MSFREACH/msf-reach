fe57d8024ce60cee20f93ca0163263acb85a9f1b
Matthew Berryman <matthew@acrossthecloud.net>
2018-11-28 09:09:10 -0800

    fix missing variable in resolve

--- src/api/routes/eventNotifications/model.js
+++ src/api/routes/eventNotifications/model.js
@@ -84,7 +84,7 @@ export default (config, db, logger) => ({
         // Execute
         logger.debug(query, values);
         db.oneOrNone(query, values).timeout(config.PGTIMEOUT)
-            .then((data) => resolve())
+            .then((data) => resolve(data))
             .catch((err) => reject(err));
     })
 
